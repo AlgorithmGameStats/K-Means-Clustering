0f538bbb2aa47b1e27f3d0af2646b4371a6dc4a8
Jumana Dakka <JumanaDakka@nbp-230-143.nbp.ruw.rutgers.edu>
2016-04-03 14:38:23 -0400

    holy hit

--- kmeans.py
+++ kmeans.py
@@ -1,7 +1,9 @@
 import os
-import numpy as np
 
-import matplotlib.pyplot as plt 
+import itertools
+
+from math import sqrt
+
 # 1) randomnly assign 3 centroids
 # 2) add data and determine distance from each data point to closest centroid
 # 3) shift new centroids by averaging data points of each cluster 
@@ -13,89 +15,84 @@ import matplotlib.pyplot as plt
 # c= # of initial clusters 
 
 
-class KMeans(Object):
+class KMeans(object):
 	"""
 	Defines the Kmeans class for our game server
-	Each point is: [time, coins, murders, level]
+	Each point is: [time, coins, murders]
 	"""
 
-	def __init__(self):
+	def __init__(self, level):
 		self.labels=['speed', 'achiever', 'killer']
-		self.centroids= [[0.0, 0.0, 0.0, ??], [], []]
-		self.clusters = [[], [], []]
-		self.dataSet = [[], [], []]
-		
 		# predefine centroid extremes
+		self.centroids= [[0.0, 0.0, 0.0], [1000.0, 1000.0, 0.0], [1000.0, 0.0, 1000.0]]
+		self.clusters = [[], [], []]
+		self.current_level=level 
 
 		# Constants
 		self.MAX_ITERATIONS=300
 
-	def put(self, item):
+	def put(self, items):
 		"""
 		Put a new entry in the dataSet,
 		Recalculate KMeans
 
 		Parameters
-    ----------
-    item : array_like
-    	[time, coins, murders, level]
+   		----------
+    	items : array_ofarrays
+    		[items]
+    		item=[time, coins, murders]
 		"""
+		# Add item to a cluster before we begin
+		self.clusters[2].extend(items)
 
-		oldCentroids = [[] for i in range(k)]
-		self.centroids=getRandomCentroids(self.dataSet, centroids, k)
+
+		oldCentroids=[[], [], []]
+		newCentroids = [[0.0, 0.0, 0.0], [1000.0, 1000.0, 0.0], [1000.0, 0.0, 1000.0]]
+		newClusters=[[], [], []]
 		iterations=0
-		oldCentroids=None
+		
 		#continue converging dataset points until max iteration
-		while not (shouldStop(oldCentroids, centroids, iterations)):
+		while not (self.__shouldStop(oldCentroids, newCentroids, iterations)):
 			iterations+=1 
-			
+			newClusters=[[], [], []]
+			oldCentroids=newCentroids[:]	#copies old centroids into new centroids
+			dataSet=itertools.chain(self.clusters[0], self.clusters[1], self.clusters[2])
+			for item in dataSet:
 
-			#assign datapoint to cluster type
-			clusters= distance(dataSet,centroids,clusters)
-			
-			#recalculate centroids as the new mean of each cluster dataset
-			index=0
-			
-			#axis=0 flatten array
-			#redefine means
-			for cluster in clusters:
-				oldCentroids[index]=centroids[index]
-				centroids[index]= map(lambda x:sum(x)/float(len(x)), zip(*dataSet)) 
-				index+=1
+				centroid_index= self.__closestcentroid(item, newCentroids) 
+				newClusters[centroid_index].append(item)
+				newCentroids[centroid_index]=self.__recalculate(newClusters[centroid_index])
 
-			labels=getLabels(centroids)
-			
 
-		return centroids
+		self.centroids=newCentroids
+	 	self.clusters=newClusters
 
-	def __distance(self):
-		for data in dataSet:
-		mu_index=array[]
-		for i in centroids:
-			mu_index.append(sqrt((data-i)**2)) 
-		mu_index= min(mu_index)
-		clusters[mu_index].append(data)
-		return clusters
-		#need to get index
+	 	return label
+	#recalculate centroids as the new mean of each cluster dataset
 
-	def __shouldStop(self, oldCentroids, iterations):
-	if iterations> MAX_ITERATIONS:
-		return True
-		return oldCentroids=centroids
-		iterations++ 
+	def __recalculate(self, cluster):
 
-	def __getRandomCentroids(dataSet, centroids, k):
-		for cluster in range(0,k):
-			centroids.append(data[np.random.randint(0, len(data), size=1].flatten(). tolist())
-		return centroids
+		return map(lambda x:sum(x)/float(len(x)), zip(*cluster))
+
+
+	def __closestcentroid(self, item, centroids):
+
+
+		return min([(i[0], sqrt(((item[0] - centroids[i[0]][0])**2) + ((item[1] - centroids[i[0]][1])**2)) + ((item[2] - centroids[i[0]][2])**2))) for i in enumerate(centroids)], key=lambda t:t[1])[0]
+
+	def __shouldStop(self, oldCentroids, centroids, iterations):
+		if iterations> self.MAX_ITERATIONS:
+			return True
+		return oldCentroids==centroids
+
 
 	def __getLabel(self, index):
 		return self.labels[index] 
 
-	
 
+	def getlabel(self,item):
 
+		index=self.__closestcentroid(item, self.centroids)
+		profile= self.__getlabel(index) 
 
-
-
-
+		return profile
