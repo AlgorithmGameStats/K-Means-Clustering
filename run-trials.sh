835995d2d0de3edfc9d680723cd5aacaccb55207
itomaldonado <mo.maldonado@gmail.com>
2016-04-18 16:47:00 -0400

    Updated trials script with.

--- run-trials.sh
+++ run-trials.sh
@@ -2,10 +2,12 @@
 
 KS="1 2 3 4 8 16"
 SIZES="10 100 1000 10000 100000 1000000"
+TRIALS="100"
+CONVERGE="0"
 
 for i in ${KS}; do
   for j in ${SIZES}; do
-    python kmeans-trials.py -c 0 -t 100 -k ${i} -n ${j}
+    python kmeans-trials.py -c ${CONVERGE} -t ${TRIALS} -k ${i} -n ${j}
     echo "Done - K: ${i}, N:${j}"
   done
 done
